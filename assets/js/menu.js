5cce225f42b479c80a71069f8f8582d87446526a
mmmaxii <mvalderrav@uc.cl>
2026-01-06 22:44:09 -0300

    v.2.0.0 Reorden de carpetas y correccion de  rutas

---
+++ assets/js/menu.js
@@ -0,0 +1,43 @@
+const botons = document.querySelectorAll(".btn");
+
+/* Reutilizamos el codigo de la calculadora que hicimos*/
+
+botons.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+        e.preventDefault();
+        /*Usamos let para poder cambiar luego y que no nos de errores */
+        let url = "";
+        let nombrePagina = "";
+
+        if (boton.id === "depositButton") {
+            url = "deposit.html";
+            nombrePagina = "Depósitos";
+        } else if (boton.id === "sendMoneyButton") {
+            url = "sendmoney.html";
+            nombrePagina = "Enviar Dinero";
+        } else if (boton.id === "transactionsButton") {
+            url = "transactions.html";
+            nombrePagina = "Transacciones";
+        }
+
+        cambiarPagina(url, nombrePagina);
+    });
+});
+
+function cambiarPagina(url, nombrePagina) {
+    /*Muestra que se redirige en la parte superior derecha*/
+    Swal.fire({
+        position: "top-end",
+        icon: "info",
+        iconColor: "#d14fd3ff",
+        // Agregando mas personalización 
+        width: "350px",
+        title: "Redirigiendo a " + nombrePagina,
+        showConfirmButton: false,
+        timer: 1000
+    });
+
+    setTimeout(() => {
+        window.location.href = url;
+    }, 1000);
+}
